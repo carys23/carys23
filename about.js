62ac2d21030f6d44fcc2629f19ce731f8a4ab56a
Carys <caryspritch@gmail.com>
2021-01-26 11:56:52 +0000

    about update

---
+++ about.js
@@ -0,0 +1,13 @@
+let name = "carys";
+let learning = ["javascript", "HTML","CSS", "Git",];
+let Iam = "photographer";
+console.log (`My name is ${name} I am ${Iam} and I am learning ${learning}`);
+let startDate = new Date ("01/11/2021");
+let todayDate = new Date ("01/26/2021");
+console.log (`I have been learning for`)
+console.log(startDate);
+console.log(todayDate);
+
+let secUntil = todayDate-startDate
+
+console.log(((((secUntil/1000)/60)/60)/24),`days.`);
